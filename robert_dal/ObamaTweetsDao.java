a5ad4a84fc48405dff81e4632e88c71219bb77f0
Robert Dragomir <rgdragomir@gmail.com>
2020-03-22 19:28:58 -0400

    changes in tweets and children - working

---
+++ robert_dal/ObamaTweetsDao.java
@@ -0,0 +1,198 @@
+package twitterEffect.dal;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import twitterEffect.model.ObamaTweets;
+import twitterEffect.model.Person;
+import twitterEffect.model.Tweets;
+
+public class ObamaTweetsDao  extends TweetsDao {
+	
+	private static ObamaTweetsDao instance = null;
+	
+	protected ObamaTweetsDao() {
+		super();
+	}
+	public static ObamaTweetsDao getInstance() {
+		if(instance == null) {
+			instance = new ObamaTweetsDao();
+		}
+		return instance;
+	}
+
+	public ObamaTweets create(ObamaTweets obamaTweets) throws SQLException {
+		// Insert into the superclass table first.
+		create(new Tweets(obamaTweets.getLinkToTweet(),obamaTweets.getTweetDate(),obamaTweets.getTweetTime(),
+						  obamaTweets.getContent(),obamaTweets.getRetweets(),obamaTweets.getPersonName()));
+
+		String insertObamaTweet = "INSERT INTO ObamaTweets(LinkToTweet,UserName,Likes,TweetImageURL) VALUES(?,?,?,?);";
+		Connection connection = null;
+		PreparedStatement insertStmt = null;
+		try {
+			connection = connectionManager.getConnection();
+			insertStmt = connection.prepareStatement(insertObamaTweet);
+			insertStmt.setString(1, obamaTweets.getLinkToTweet());
+			insertStmt.setString(2, obamaTweets.getUserName());
+			insertStmt.setInt(3, obamaTweets.getLikes());
+			insertStmt.setString(4, obamaTweets.getTweetImageURL());
+			insertStmt.executeUpdate();
+			return obamaTweets;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(insertStmt != null) {
+				insertStmt.close();
+			}
+		}
+	}
+
+	/**
+	 * Update the Content of the ObamaTweets instance.
+	 * This runs a UPDATE statement.
+	 */
+	public ObamaTweets updateContent(ObamaTweets obamaTweet, String newContent) throws SQLException {
+		// The field to update only exists in the superclass table, so we can
+		// just call the superclass method.
+		super.updateContent(obamaTweet, newContent);
+		return obamaTweet;
+	}
+
+	/**
+	 * Delete the TrumpTweets instance.
+	 * This runs a DELETE statement.
+	 */
+	public ObamaTweets delete(ObamaTweets obamaTweet) throws SQLException {
+		String deleteObamaTweet = "DELETE FROM ObamaTweets WHERE LinkToTweet=?;";
+		Connection connection = null;
+		PreparedStatement deleteStmt = null;
+		try {
+			connection = connectionManager.getConnection();
+			deleteStmt = connection.prepareStatement(deleteObamaTweet);
+			deleteStmt.setString(1, obamaTweet.getLinkToTweet());
+			int affectedRows = deleteStmt.executeUpdate();
+			if (affectedRows == 0) {
+				throw new SQLException("No records available to delete for LinkToTweet=" + obamaTweet.getLinkToTweet());
+			}
+			super.delete(obamaTweet);
+			return null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(deleteStmt != null) {
+				deleteStmt.close();
+			}
+		}
+	}
+
+	public ObamaTweets getObamaTweetsByLinkToTweet(String linkToTweet) throws SQLException {
+		// To build an BlogUser object, we need the Persons record, too.
+		String selectObamaTweet =
+			"SELECT * " +
+			"FROM ObamaTweets INNER JOIN Tweets " +
+			"  ON ObamaTweets.LinkToTweets = Obama.LinkToTweets " +
+			"WHERE ObamaTweets.LinkToTweets=?;";
+		Connection connection = null;
+		PreparedStatement selectStmt = null;
+		ResultSet results = null;
+		try {
+			connection = connectionManager.getConnection();
+			selectStmt = connection.prepareStatement(selectObamaTweet);
+			selectStmt.setString(1, linkToTweet);
+			results = selectStmt.executeQuery();
+			PersonDao personsDao = PersonDao.getInstance();
+			if(results.next()) {
+				String resultLinkToTweet = results.getString("LinkToTweet");
+				Date tweetDate = results.getDate("TweetDate");
+				Timestamp tweetTime = results.getTimestamp("TweetTime");
+				String content = results.getString("Content");
+				int retweets = results.getInt("Retweets");			
+				String personName = results.getString("PersonName");
+				Person person = personsDao.getPersonByPersonName(personName);
+				String userName = results.getString("UserName");
+				int likes = results.getInt("Likes");
+				String tweetImageURL = results.getString("TweetImageURL");
+				ObamaTweets obamaTweet = new ObamaTweets(resultLinkToTweet,tweetDate,tweetTime,content,retweets,person,
+														userName,likes,tweetImageURL);
+				return obamaTweet;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(selectStmt != null) {
+				selectStmt.close();
+			}
+			if(results != null) {
+				results.close();
+			}
+		}
+		return null;
+	}
+
+	public List<ObamaTweets> getObamaTweetsByDate(Date tweetDate)
+			throws SQLException {
+		List<ObamaTweets> obamaTweets = new ArrayList<ObamaTweets>();
+		String selectObamaTweets =
+			"SELECT * " +
+			"FROM ObamaTweets INNER JOIN Tweets " +
+			"  ON ObamaTweets.LinkToTweet = Tweets.LinkToTweets " +
+			"WHERE Tweets.TweetDate=?;";
+		Connection connection = null;
+		PreparedStatement selectStmt = null;
+		ResultSet results = null;
+		try {
+			connection = connectionManager.getConnection();
+			selectStmt = connection.prepareStatement(selectObamaTweets);
+			selectStmt.setDate(1, tweetDate);
+			results = selectStmt.executeQuery();
+			PersonDao personsDao = PersonDao.getInstance();
+			while(results.next()) {
+				String linkToTweet = results.getString("LinkToTweet");
+				Date resultTweetDate = results.getDate("TweetDate");
+				Timestamp tweetTime = results.getTimestamp("TweetTime");
+				String content = results.getString("Content");
+				int retweets = results.getInt("Retweets");			
+				String personName = results.getString("PersonName");
+				Person person = personsDao.getPersonByPersonName(personName);
+				String userName = results.getString("UserName");
+				int likes = results.getInt("Likes");
+				String tweetImageURL = results.getString("TweetImageURL");
+				ObamaTweets obamaTweet = new ObamaTweets(linkToTweet,resultTweetDate,tweetTime,content,retweets,person,
+														 userName,likes,tweetImageURL);
+				obamaTweets.add(obamaTweet);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(selectStmt != null) {
+				selectStmt.close();
+			}
+			if(results != null) {
+				results.close();
+			}
+		}
+		return obamaTweets;
+	}
+}
---
+++ robert_dal/TweetsDao.java
@@ -0,0 +1,219 @@
+package twitterEffect.dal;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import twitterEffect.model.Person;
+import twitterEffect.model.Tweets;
+
+public class TweetsDao {
+	
+	protected ConnectionManager connectionManager;
+	
+	// Single pattern: instantiation is limited to one object.
+	private static TweetsDao instance = null;
+	protected TweetsDao() {
+		connectionManager = new ConnectionManager();
+	}
+	public static TweetsDao getInstance() {
+		if(instance == null) {
+			instance = new TweetsDao();
+		}
+		return instance;
+	}
+
+	/**
+	 * Save Tweets instance by storing it in your MySQL instance.
+	 * This runs a INSERT statement.
+	 */
+	public Tweets create(Tweets tweet) throws SQLException {
+		String insertTweet = 
+				"INSERT INTO Tweets(LinkToTweet,TweetDate,TweetTime,Content,Retweets,PersonName) " +
+				"VALUES(?,?,?,?,?,?);";
+		Connection connection = null;
+		PreparedStatement insertStmt = null;
+		try {
+			connection = connectionManager.getConnection();
+			insertStmt = connection.prepareStatement(insertTweet);
+			insertStmt.setString(1, tweet.getLinkToTweet());
+			insertStmt.setDate(2, new java.sql.Date(tweet.getTweetDate().getTime()));
+			insertStmt.setTimestamp(3, new Timestamp(tweet.getTweetTime().getTime()));
+			insertStmt.setString(4, tweet.getContent());
+			insertStmt.setInt(5, tweet.getRetweets());
+			insertStmt.setString(6, tweet.getPersonName().getPersonName());
+			insertStmt.executeUpdate();
+			return tweet;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(insertStmt != null) {
+				insertStmt.close();
+			}
+		}
+	}
+
+	/**
+	 * Update the Content of the Tweets instance.
+	 * This runs a UPDATE statement.
+	 */
+	public Tweets updateContent(Tweets tweet, String content) throws SQLException {
+		String updateTweet = "UPDATE Tweets SET Content=? WHERE LinkToTweet=?;";
+		Connection connection = null;
+		PreparedStatement updateStmt = null;
+		try {
+			connection = connectionManager.getConnection();
+			updateStmt = connection.prepareStatement(updateTweet);
+			updateStmt.setString(1, content);
+			updateStmt.setString(2, tweet.getLinkToTweet());
+			updateStmt.executeUpdate();
+			
+			// Update the person param before returning to the caller.
+			tweet.setContent(content);;
+			return tweet;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(updateStmt != null) {
+				updateStmt.close();
+			}
+		}
+	}
+
+	/**
+	 * Delete the Persons instance.
+	 * This runs a DELETE statement.
+	 */
+	public Tweets delete(Tweets tweet) throws SQLException {
+		String deleteTweet = "DELETE FROM Tweets WHERE LinkToTweet=?;";
+		Connection connection = null;
+		PreparedStatement deleteStmt = null;
+		try {
+			connection = connectionManager.getConnection();
+			deleteStmt = connection.prepareStatement(deleteTweet);
+			deleteStmt.setString(1, tweet.getLinkToTweet());
+			deleteStmt.executeUpdate();
+
+			// Return null so the caller can no longer operate on the Persons instance.
+			return null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(deleteStmt != null) {
+				deleteStmt.close();
+			}
+		}
+	}
+
+	/**
+	 * Get the Tweets record by fetching it from your MySQL instance.
+	 * This runs a SELECT statement and returns a single Tweets instance.
+	 */
+	public Tweets getTweetsByLinkToTweet(String linkToTweet) throws SQLException {
+		String selectTweet = 
+				"SELECT * " +
+				"FROM Tweets " +
+				"WHERE LinkToTweet=?;";
+		Connection connection = null;
+		PreparedStatement selectStmt = null;
+		ResultSet results = null;
+		try {
+			connection = connectionManager.getConnection();
+			selectStmt = connection.prepareStatement(selectTweet);
+			selectStmt.setString(1, linkToTweet);
+			results = selectStmt.executeQuery();
+			PersonDao personsDao = PersonDao.getInstance();
+			if(results.next()) {
+				String resultLinkToTweet = results.getString("LinkToTweet");
+				Date tweetDate = results.getDate("TweetDate");
+				Timestamp tweetTime = results.getTimestamp("TweetTime");
+				String content = results.getString("Content");
+				int retweets = results.getInt("Retweets");		
+				String personName = results.getString("PersonName");
+				Person person = personsDao.getPersonByPersonName(personName);
+				Tweets tweet = new Tweets(resultLinkToTweet,tweetDate,tweetTime,content,retweets,person);
+				return tweet;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(selectStmt != null) {
+				selectStmt.close();
+			}
+			if(results != null) {
+				results.close();
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Get the matching Tweets records by fetching from your MySQL instance.
+	 * This runs a SELECT statement and returns a list of matching Persons.
+	 */
+	public List<Tweets> getTweetsByPersonName(String personName) throws SQLException {
+		List<Tweets> tweets = new ArrayList<Tweets>();
+		String selectTweets =
+			"SELECT LinkToTweet,TweetDate,TweetTime,Content,Retweets,PersonName " + 
+			"FROM Tweets " + 
+			"WHERE PersonName=?;";
+		Connection connection = null;
+		PreparedStatement selectStmt = null;
+		ResultSet results = null;
+		try {
+			connection = connectionManager.getConnection();
+			selectStmt = connection.prepareStatement(selectTweets);
+			selectStmt.setString(1, personName);
+			results = selectStmt.executeQuery();
+			PersonDao personsDao = PersonDao.getInstance();
+			while(results.next()) {
+				String linkToTweet = results.getString("LinkToTweet");
+				Date tweetDate = results.getDate("TweetDate");
+				Timestamp tweetTime = results.getTimestamp("TweetTime");
+				String content = results.getString("Content");
+				int retweets = results.getInt("Retweets");			
+				String resultPersonName = results.getString("PersonName");
+				Person person = personsDao.getPersonByPersonName(resultPersonName);
+				Tweets tweet = new Tweets(linkToTweet,tweetDate,tweetTime,content,retweets,person);
+				tweets.add(tweet);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(selectStmt != null) {
+				selectStmt.close();
+			}
+			if(results != null) {
+				results.close();
+			}
+		}
+		return tweets;
+	}
+
+}
